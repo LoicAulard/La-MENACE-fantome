2b2565434d328728ffa1df337c5ed9867a5a8b1a
LoicAulard <35135872+LoicAulard@users.noreply.github.com>
2018-01-05 17:51:06 +0100

    menace0.1.c

--- menace.c
+++ menace.c
@@ -7,45 +7,14 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-int set_case(int terrain, int cases, int value) // cases diot etre une des constant CASEn; value doit etre CROIX ROND ou VIDE
+int set_case(int terrain, int cases, int value) // cases diot etre le numéro de la case; value doit etre CROIX ROND ou VIDE
 {
-  switch (cases)
-  {
-    case CASE1: return terrain|value;
-    break;
-
-    case CASE2: return terrain|(value<<2);
-    break;
-
-    case CASE3: return terrain|(value<<4);
-    break;
-
-    case CASE4: return terrain|(value<<6);
-    break;
-
-    case CASE5: return terrain|(value<<8);
-    break;
-
-    case CASE6: return terrain|(value<<10);
-    break;
-
-    case CASE7: return terrain|(value<<12);
-    break;
-
-    case CASE8: return terrain|(value<<14);
-    break;
-
-    case CASE9: return terrain|(value<<16);
-    break;
-
-    default: fprintf(stderr,"Cette case n'existe pas.");
-    exit(-1);
-  }
+    return terrain|(value<<((case-1)*2));
 }
 
-int get_case(int terrain, int cases)// cases diot etre une des constant CASEn;
+int get_case(int terrain, int cases)// cases diot etre une des constant le numéro de la case;
 {
-  return (terrain&cases)/(cases/3);
+  return (terrain&(3<<((case-1)*2)))/(cases/3);
 }
 
 int nb_case_libre(int terrain)
